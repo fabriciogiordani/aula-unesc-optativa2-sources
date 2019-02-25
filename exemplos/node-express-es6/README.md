3227a029641bd1b158e579914f601aef8d83f35e
Fabricio Giordani <fgiordani@gmail.com>
2019-02-25 15:16:12 -0300

    no message

---
+++ exemplos/node-express-es6/README.md
@@ -0,0 +1,20 @@
+# Exemplo Nodejs + Express + TS
+
+# Preparando o projeto
+
+```
+npm install
+```
+
+# Executando o projeto
+
+```
+ts-node index.ts
+```
+
+_É necessário ter o ts-node instalado. Caso não tenha:_
+
+```
+npm install -g ts-node
+```
+
---
+++ exemplos/node-express-es6/index.ts
@@ -0,0 +1,12 @@
+import * as express from 'express'
+
+const port = 3000
+const app = express()
+
+app.get('/', (req, res) => {
+  res.json({status: 'Server is running!'})
+})
+
+app.listen(port, () => {
+  console.log(`Server is running at localhost:${port}`)
+})
